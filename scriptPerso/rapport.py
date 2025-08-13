daa8ae1bb649098c15f4c24961a32f3bd166bec3
baris <baris.ock.etudes@gmail.com>
2025-08-13 20:05:01 +0200

    ajout pre/post pour generer_rapport

--- scriptPerso/rapport.py
+++ scriptPerso/rapport.py
@@ -1,6 +1,27 @@
 import pandas as pd
 
 def generer_rapport(fichier_csv):
+    """
+    Préconditions :
+    - fichier_csv existe et est lisible
+    - Le fichier contient au minimum les colonnes :
+      'Categorie', 'Quantite', 'Prix unitaire'
+
+    Postconditions :
+    - Le dictionnaire retourné contient :
+        {
+            "quantite_par_categorie": pandas.Series,
+            "valeur_totale": float,
+            "produit_cher": pandas.Series
+        }
+
+    Invariant :
+    - Le fichier source reste inchangé
+
+    Exceptions :
+    - FileNotFoundError : fichier introuvable
+    - KeyError : colonnes attendues manquantes
+    """
     df = pd.read_csv(fichier_csv, sep=';', encoding='utf-8')
     quantite_par_categorie = df.groupby("Categorie")["Quantite"].sum()
     df['Valeur totale'] = df['Quantite'] * df['Prix unitaire']
