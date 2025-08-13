c99a495945be46012929144660eaca045d8b5ff8
baris <baris.ock.etudes@gmail.com>
2025-08-13 19:22:14 +0200

    ajout de précondition et postcondition sur filtre_par_prix

--- scriptPerso/recherche.py
+++ scriptPerso/recherche.py
@@ -9,5 +9,16 @@ def recherche_categorie(nom_categorie, fichier_csv):
     return df[df['Categorie'].str.contains(nom_categorie, case=False, na=False)]
 
 def filtre_par_prix(prix_min, prix_max, fichier_csv):
+    """
+    Filtre les produits selon un intervalle de prix 
+
+    Pré :
+    - le fichier_csv existe
+    - 0 <= prix_min <= prix_max
+    - Colonne 'Prix unitaire' est présente
+
+    Post : 
+    - Toutes les lignes retournées ont un prix compris entre [prix_min, prix_max]
+    """
     df = pd.read_csv(fichier_csv, sep=';', encoding='utf-8')
     return df[(df['Prix unitaire'] >= prix_min) & (df['Prix unitaire'] <= prix_max)]
