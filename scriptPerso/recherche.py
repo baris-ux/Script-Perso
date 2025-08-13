2673fab1f4e5cea36147506f2c7ccf0c9fc7b844
baris <baris.ock.etudes@gmail.com>
2025-08-13 19:55:07 +0200

    ajout de pre/post pour recherche_produits

--- scriptPerso/recherche.py
+++ scriptPerso/recherche.py
@@ -1,6 +1,23 @@
 import pandas as pd
 
 def recherche_produit(nom_produit, fichier_csv):
+    """
+    Recherche les produits par nom dans un fichier CSV
+
+    Préconditions : 
+    - fichier_csv existe et est lisible
+     - Le fichier contient la colonne 'Nom_du_produit'
+    - nom_produit est une chaine non vide
+    
+    Postconditions : 
+    - Le type de retour est un pandas.DataFrame (même si vide)
+    - Le DataFrame retourné contient uniquement des lignes avec 'Nom_du_produit' correspondant au filtre
+
+    Exceptions :
+    - FileNotFoundError : fichier introuvable
+    - KeyError : colonne 'Nom_du_produit' manquante
+    
+    """
     df = pd.read_csv(fichier_csv, sep=';', encoding='utf-8')
     return df[df['Nom_du_produit'].str.contains(nom_produit, case=False, na=False)]
 
